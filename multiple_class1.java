aec1c95258750bdabe09d79116c5d6113dbad6c0
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-08-25 22:48:50 +0530

    Multiple classes

---
+++ multiple_class1.java
@@ -0,0 +1,12 @@
+public class multiple_class1
+{
+
+    static void Add()
+    {
+        System.out.println("This is 1st file");
+    }
+    public static void main(String[] args) 
+    {
+       multiple_class2.Sub();
+    }
+}
---
+++ multiple_class2.java
@@ -0,0 +1,11 @@
+public class multiple_class2 
+{
+    public static void Sub() 
+    {
+        System.out.println("This is 2nd file");
+    }
+    public static void main(String[] args) 
+    {
+        multiple_class1.Add();
+    }
+}
