22becd2143bb926a0feb4635945886929b52756c
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-09-10 17:51:39 +0530

    Creating user defined packages

---
+++ Basic_Calculator/src/arithmetic/java/Basic_calc.java
@@ -0,0 +1,27 @@
+package arithmetic.java;
+
+public class Basic_calc 
+{
+    int c;
+    public  int Add(int x,int y)
+    {
+        return c=x+y;
+    }
+    public int Sub(int x,int y)
+    {
+        return c=x-y;
+    }
+    public void Mul(int x,int y)
+    {
+        c=x*y;
+    }
+    public void Div(int x,int y)
+    {
+        c=x/y;
+    }
+    public void Rem(int x,int y)
+    {
+        c=x%y;
+    }
+
+}
